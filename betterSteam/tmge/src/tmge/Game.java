b7a4d90733f401a81e69d11b5d3412fd9e3a4034
fevkebede <fevkebede@gmail.com>
2021-03-14 10:27:35 -0700

    add game abstract class

--- betterSteam/tmge/src/tmge/Game.java
+++ betterSteam/tmge/src/tmge/Game.java
@@ -1,13 +1,20 @@
 package tmge;
 
+import java.util.ArrayList;
+import java.util.function.Function;
+
 import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.SimpleIntegerProperty;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 
 public abstract class Game {
-	public IntegerProperty score = new SimpleIntegerProperty();
-//	Grid grid;
+	protected PlayerData player;
+	protected Grid grid;
+	
+	protected IntegerProperty score = new SimpleIntegerProperty();
+	
+	protected Function<Integer, Integer> onGameEnd;
 	
 //	public abstract void update();
 	
@@ -20,9 +27,11 @@ public abstract class Game {
 //	public abstract void save();
 	public abstract void quit();
 	
-//	void swapCell(Tile a, Tile b) {
-//		
-//	}
+	
+	
+	void swapTiles(Tile a, Tile b) {
+		
+	}
 //	
 //	void checkMatch(Tile a, Tile b) {
 //		
